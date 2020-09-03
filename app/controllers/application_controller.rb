7515c8d377e694366c3f66548dd653c07868b328
Garrett Thompson <gtcruz.gt@gmail.com>
2020-09-03 22:43:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
     if params[:username].empty?
       redirect to '/failure'
     end
-    
+
     user = User.new(:username => params[:username], :password => params[:password])
 
 	  if user.save
