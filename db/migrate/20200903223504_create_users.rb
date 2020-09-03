3cfc9b366a142bae94c1a534eb6c540d2191966e
Garrett Thompson <gtcruz.gt@gmail.com>
2020-09-03 22:47:47 +0000

    Automatically backed up by Learn

--- db/migrate/20200903223504_create_users.rb
+++ db/migrate/20200903223504_create_users.rb
@@ -1,4 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+    end
+  end
+  
   def up
     create_table :users do |t|
       t.string :username
